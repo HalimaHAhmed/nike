d21f77caba95062062e342fea18387e8eae63557
HalimaHAhmed <hanahussein6155@gmail.com>
2023-12-18 16:25:10 +0300

    building nike project using tailwind css

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import specialOffer from './SpecialOfer'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    specialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
